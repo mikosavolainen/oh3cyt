35e591941641fad8d13babee65ec680764e20192
mikosavolainen <miko.markus2112@gmail.com>
2024-01-19 23:17:07 +0200

    analytiikka

---
+++ analytics-ads.js
@@ -0,0 +1,22 @@
+// analytics-ads.js
+
+(function () {
+    // Google Analytics
+    var trackingID = 'G-12E7VJ916H';
+
+    // Lisää Google Analyticsin gtag-koodi
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=' + trackingID;
+    document.head.appendChild(script);
+
+    script.onload = function () {
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+        gtag('config', trackingID);
+    };
+
+    // Google Ads
+    gtag('event', 'conversion', {'send_to': 'AW-XXXXXXXXX/YOUR_CONVERSION_ID'});
+})();
